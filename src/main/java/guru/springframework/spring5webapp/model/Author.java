cd42ebf25a47e10ba049b89ca2279a9abba950d7
zainhabib <zain.n.habib@gmail.com>
2022-08-18 12:35:36 -0500

    Add MVC -- controller, thymeleaf

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -77,7 +77,6 @@ public class Author {
                 "id=" + id +
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 }
